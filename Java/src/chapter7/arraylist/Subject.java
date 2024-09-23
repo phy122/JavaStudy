994d954d6e88c856b88d687b62e4fc3eb9589024
phy122 <qkrghdud369@gmail.com>
2024-09-23 23:27:26 +0900

    배열 마무리

---
+++ Java/src/chapter7/arraylist/Subject.java
@@ -0,0 +1,23 @@
+package chapter7.arraylist;
+
+public class Subject {
+    String name;
+    int scorePoint;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScorePoint() {
+        return this.scorePoint;
+    }
+
+    public void setScorePoint(int scorePoint) {
+        this.scorePoint = scorePoint;
+    }
+    
+}
